c5768ace387eadbb83e89f725e1ac7b139c27e30
calvillovaleria49-puntocom <calvillovaleria49@gmail.com>
2026-06-30 17:19:56 -0600

    security: configurar HTTPS, cookies seguras y cabeceras HTTP para reforzar la seguridad

---
+++ frontend/src/App.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Dashboard from './pages/Dashboard';
+import CRM from './pages/CRM';
+import Inventario from './pages/Inventario';
+import Cotizacion from './pages/Cotizacion';
+import AvisoPrivacidad from './components/AvisoPrivacidad';
+import './index.css';
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/crm" element={<CRM />} />
+        <Route path="/inventario" element={<Inventario />} />
+        <Route path="/cotizacion" element={<Cotizacion />} />
+        <Route path="/aviso-privacidad" element={<AvisoPrivacidad />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
